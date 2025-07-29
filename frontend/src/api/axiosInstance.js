6b8db7d09c1ef28f896357825632d05f58f43c99
Sebastián Sinchi Naula <151038925+elsebasdev1@users.noreply.github.com>
2025-07-29 16:22:20 -0500

    Update axiosInstance to use remote server

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8081/backend/rest',
+  baseURL: 'http://52.250.80.229:8081/backend/rest',
   headers: {
     'Content-Type': 'application/json',
   },
