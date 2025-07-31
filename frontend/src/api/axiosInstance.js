dc7fc63b8e3bcc25b14618e59d140ca478bbbef5
Sebastián Sinchi Naula <151038925+elsebasdev1@users.noreply.github.com>
2025-07-30 21:07:03 -0500

    Update axiosInstance.js

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://52.250.80.229:8081/backend/rest',
+  baseURL: 'https://azuclinic.duckdns.org/backend/rest',
   headers: {
     'Content-Type': 'application/json',
   },
